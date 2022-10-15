a1b55b223bdddf5eed9c03c423a0c0579d477c4b
Darrion Banks <dbanks2500@gmail.com>
2022-10-14 23:24:51 -0400

    Add fields to base user model.

--- client-manager/src/main/java/io/darrion/clientmanager/model/UserModel.java
+++ client-manager/src/main/java/io/darrion/clientmanager/model/UserModel.java
@@ -1,4 +1,17 @@
 package io.darrion.clientmanager.model;
 
+import lombok.Getter;
+import lombok.Setter;
 
-public interface UserModel {}
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class UserModel {
+    Integer id;
+    String firstName;
+    String middleName;
+    String lastName;
+    Timestamp createdAt;
+    Timestamp updatedAt;
+}
