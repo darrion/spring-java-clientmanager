5600f955ea0966f16a6495406366d06285a2c040
Darrion Banks <dbanks2500@gmail.com>
2022-10-15 03:03:11 -0400

    Setup advisor and client table representations. Setup advisor model factory.

--- client-manager/src/main/java/io/darrion/clientmanager/table/AdvisorTable.java
+++ client-manager/src/main/java/io/darrion/clientmanager/table/AdvisorTable.java
@@ -3,6 +3,8 @@ package io.darrion.clientmanager.table;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Timestamp;
+
 import javax.persistence.*;
 
 @Getter
@@ -12,6 +14,21 @@ public class AdvisorTable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
-    private Long id;
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName; 
+
+    @Column(name = "middle_name", nullable = false)
+    private String middleName; 
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName; 
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt; 
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updatedAt; 
 
 }
