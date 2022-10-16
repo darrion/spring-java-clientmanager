cf67161fc067264864d6ebab53f324218766f8ec
Darrion Banks <dbanks2500@gmail.com>
2022-10-16 18:15:10 -0400

    Simple endpoint logic check.

---
+++ client-manager/src/main/java/io/darrion/clientmanager/model/Assignment.java
@@ -0,0 +1,12 @@
+package io.darrion.clientmanager.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Assignment {
+
+    Integer clientId;
+    Integer advisorId; 
+}
