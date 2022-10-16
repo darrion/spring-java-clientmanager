d9ba6f5a67b67ddfc8fef5ce7c7d36a2bf01639d
Darrion Banks <dbanks2500@gmail.com>
2022-10-16 18:15:10 -0400

    Simple endpoint logic check.

--- client-manager/src/main/java/io/darrion/clientmanager/controller/AdvisorController.java
+++ client-manager/src/main/java/io/darrion/clientmanager/controller/AdvisorController.java
@@ -1,16 +1,23 @@
 package io.darrion.clientmanager.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.darrion.clientmanager.entity.AdvisorEntity;
 import io.darrion.clientmanager.model.Advisor;
+import io.darrion.clientmanager.model.Assignment;
 
 @RestController("/advisor")
 public class AdvisorController {
 
-    @PostMapping("/assign")
-    public AdvisorEntity assign(Integer clientId, Advisor advisor) {
-        return null;
+    @PostMapping(value = "/assign", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> assign(@RequestBody Assignment assignment) {
+        return ResponseEntity.ok(assignment);
     }
 }
---
+++ client-manager/src/main/java/io/darrion/clientmanager/factory/ClientFactory.java
@@ -0,0 +1,3 @@
+package io.darrion.clientmanager.factory;
+
+public class ClientFactory {}
